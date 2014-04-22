2897c2b32097890062f36759eaef490ba4f46f80
Frison Alexander <soggyfri@gmail.com>
2014-04-22 17:54:59 -0400

    menu css done. on to js

--- azure-demo/Scripts/app/app.menu.js
+++ azure-demo/Scripts/app/app.menu.js
@@ -1,13 +1,20 @@
 ﻿var AppMenu = function AppMenu() {
 	var self = this;
-        
+    
 	self.load = function (data) {
-	    //debugger;
-
 	    ko.applyBindings(data.SubMenu, document.getElementById('HomeContainer'));
 	};
+
+	function createDOMLink() {
+	    var $root = $("#dl-menu");
+	}
 
 	return self;
 };
 
 var AppMenuInstance = new AppMenu();
+
+//Request menu on refresh, to save one click during debugging
+$.get("/api/ui/getmenu")
+ .done(AppMenuInstance.load)
+ .fail(function () { alert("error loading menu"); });
